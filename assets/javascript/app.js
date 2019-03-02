4e5326a1ec1a118d93ca786e7be58315406ecd67
Steve Hoang <steve.n.hoang90@gmail.com>
2019-03-02 01:27:57 -0800

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,66 @@
 var categories=["doge", "dance","mr robot", "star wars","snake", "porter robinson","edc","fixed gear", ];
 
 
-   //button functions
+//    button functions
 
-    // function loadButtons(){
-    //     $(".button-view").empty();
-    //     for( var i =0; i < categories.length; i ++){
-    //         var newButton = $("<button>");
-    //         newButton.addClass("category btn btn-default");
-    //         newButton.attr("data-name", categories[i]);
-    //         newButton.text(categories[i]);
-    //         $(".button-view").append(newButton);
-    //     }
-    // };
+    function loadButtons(){
+        $(".button-view").empty();
+        for( var i =0; i < categories.length; i ++){
+            var newButton = $("<button>");
+            newButton.addClass("category btn btn-default");
+            newButton.attr("data-name", categories[i]);
+            newButton.text(categories[i]);
+            $(".button-view").append(newButton);
+        }
+    };
 
-    // $("#add-category").on("click", function(event){
-    //     e.preventDefault();
-    //     var topic= $("#search-input").val().toLowerCase().trim();
-    //     var queryURL = "";
+    $("#add-category").on("click", function(e){
+        e.preventDefault();
+        var topic= $("#search-input").val().toLowerCase().trim();
+        var queryURL = "";
 
-    //     $.ajax({
-    //         url:queryURL,
-    //         method: "GET"
-    //     }).then(function(response){
-    //         if(response.data.length == 0){
-    //             alert("No results found.");
-    //         }
-    //         else if(categories.indexOf(topic) != -1){
-    //             alert("This topic already exists.")
-    //         }
-    //         else{
-    //             categories.push(topic);
-    //             loadButtons();
-    //         }
-    //     });
-    // });
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){
+            if(response.data.length == 0){
+                alert("No results found.");
+            }
+            else if(categories.indexOf(topic) != -1){
+                alert("This topic already exists.")
+            }
+            else{
+                categories.push(topic);
+                loadButtons();
+            }
+        });
+    });
 
    function displayGIF(){
-    //    var category = $(this).attr("data-name");
-       var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Xh78ijfyRDmeQ9KYjKZqKOj0oQStEmRp";
+    var category = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ category + "api_key=Xh78ijfyRDmeQ9KYjKZqKOj0oQStEmRp";
 
-       $.ajax({
+    $.ajax({
            url: queryURL,
            method: "GET"
        }).then(function(response){
-           console.log(response);
-        //    $(".giphy-view").empty();
-        //    for(var i = 0 ; i < response.data.lenght;i++){
-        //        var gifDiv= $("<div>");
-        //        gifDiv.addClas("gifDiv");
+           
+        console.log(response);
+        $(".giphy-view").empty();
+        for(var i = 0; i < response.data.length; i++){
+            var gifDiv = $("<div>");
+            gifDiv.addClass("gifDiv");
+            gifDiv.html("<p>Rating: " + response.data[i].rating.toUpperCase() +"</p>");
+
+            var gifImage = $("<img src='" + response.data[i].images.fixed_height_still.url + "'>");
+            gifImage.addClass("gif");
+
+            var imageDiv = $("<div>");
+            imageDiv.addClass("play");
+            imageDiv.attr("data-state", "still");
+            imageDiv.
             
 
-        //    }
+           }
        });
    };
